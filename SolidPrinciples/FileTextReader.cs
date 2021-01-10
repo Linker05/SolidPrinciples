2bfbb2d3b174cb134472308ddbe5e5caab9f44cb
Сизиков Толя <tolya2005sizikov@mail.ru>
2021-01-10 14:25:57 +0700

    Добавьте файлы проекта.

---
+++ SolidPrinciples/FileTextReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples
+{
+    public class FileTextReader
+    {
+        public FileTextReader()
+        {
+
+        }
+
+        public string ReadText(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open))
+            using (var sr = new StreamReader(fs))
+                return sr.ReadToEnd();
+        }
+    }
+}
---
+++ SolidPrinciplesConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace SolidPrinciplesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fs = File.OpenRead("text.txt");
+            var sr = new StreamReader(fs);
+
+            Console.WriteLine(sr.ReadToEnd());
+        }
+    }
+}
